c175e01dacb6ea869b2098b3d90b1be1a5383e1a
adavidys <133536771+adavidys@users.noreply.github.com>
2024-10-07 16:03:47 +0300

    jklkljkl
    jkljkljkl

--- plugins/naurok/naurok.py
+++ plugins/naurok/naurok.py
@@ -1,3 +1,15 @@
+import time
+
+import numpy
+import asyncio
+import threading
+
+import aiohttp
+import multiprocessing
+
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
 class Load_img():
 	def __init__(self):
 		super().__init__()
@@ -5,6 +17,41 @@ class Load_img():
 class Load_data():
 	def __init__(self):
 		super().__init__()
+	
+	def search_url(self, pages, text="", subject="", klas="0"):
+		self.url_list = []
+		async def get_test(session, page, text=text, subject=subject, klas=klas):
+			if subject != "": subject = f"/{subject}"
+
+			async with session.get(f"https://naurok.com.ua/test{subject}/klas-{klas}/storinka-{page}?q={text}") as req:
+				soup = BeautifulSoup(await req.text(), "lxml")
+			for item in soup.find_all(class_="headline"):
+				self.url_list.append("https://naurok.com.ua"+item.find("a").get("href"))
+
+		async def async_run():
+			async with aiohttp.ClientSession(headers={"user-agent": UserAgent().random}) as session:
+				task = [get_test(session, page) for page in range(1, pages+1)]
+				await asyncio.gather(*task)
+
+				print(self.url_list) 
+				
+
+		start = time.perf_counter()
+		asyncio.run(async_run())
+		print(time.perf_counter()-start)
+
+a = Load_data()
+a.search_url(3, text="", subject="", klas="0")
+
+
+
+
+
+
+
+
+
+
 
 
 class Main():
