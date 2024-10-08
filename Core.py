45ef90134edff9e977bd8fb2cbb78ff58a764678
69BooM96 <tankionlaun524@gmail.com>
2024-10-08 17:45:57 +0300

    GUI 04

--- Core.py
+++ Core.py
@@ -8,10 +8,26 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QThread, QProcess
 import psutil
 import sys
+import random
 from modules import ld_plugins
 from modules import sr_data
 from modules import GUI
+from modules import GUI_update
 
+
+class Core_load(QtWidgets.QMainWindow, GUI_update.Ui_MainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+		self.logs = ExampleApp().logs
+		self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+		path = f'res/gif/giphy ({random.randint(0, 40)}).webp'
+		gif = QtGui.QMovie(path)  # !!!
+		self.label_2.setMovie(gif)
+		gif.start()
+
+		self.logs("INFO", "START LOAD")
 
 class ExampleApp(QtWidgets.QMainWindow, GUI.Ui_MainWindow):
 	def __init__(self):
@@ -19,10 +35,8 @@ class ExampleApp(QtWidgets.QMainWindow, GUI.Ui_MainWindow):
 		self.setupUi(self)
 
 		self.logs("INFO", "START")
+		print("eqweqwe")
 
-		list_update = ld_plugins.check_pl(self)
-		if len(list_update) != 0:
-			ld_plugins.update_pl(self, list_update)
 
 	def logs(self, type_log: Literal["info", "INFO", "WARN", "ERROR"], theme_log="none", text_log=""):
 		data_log = f'[{time.strftime("%H:%M:%S")}] <{type_log}> [{theme_log}]{text_log}'
@@ -32,18 +46,18 @@ class ExampleApp(QtWidgets.QMainWindow, GUI.Ui_MainWindow):
 
 
 if __name__ == '__main__':
-    start_core = 0
+	start_core = 0
 
-    for proc in psutil.process_iter():
-        name = proc.name()
-        if name == "Test_Finder.exe":
-            start_core += 1
+	for proc in psutil.process_iter():
+		name = proc.name()
+		if name == "Test_Finder.exe":
+			start_core += 1
 
-    if start_core < 2:
-        app = QtWidgets.QApplication(sys.argv)
-        window = ExampleApp()
-        window.show()
-        sys.exit(app.exec_())
-    else:
-        pass
+	if start_core < 2:
+		app = QtWidgets.QApplication(sys.argv)
+		window = Core_load()
+		window.show()
+		sys.exit(app.exec_())
+	else:
+		pass
 	
