203a37eac1769d1137d2afdb57089e6ffcd32550
69BooM96 <tankionlaun524@gmail.com>
2024-10-26 13:07:10 +0300

    GUI

--- Core.py
+++ Core.py
@@ -19,23 +19,21 @@ class Core_load(QtWidgets.QMainWindow, GUI_update.Ui_MainWindow):
 	def __init__(self):
 		super().__init__()
 		self.setupUi(self)
-		self.logs = ExampleApp().logs
 		self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 
 		path = f'res/gif/giphy ({random.randint(0, 40)}).webp'
-		gif = QtGui.QMovie(path)  # !!!
+		gif = QtGui.QMovie(path)
 		self.label_2.setMovie(gif)
 		gif.start()
 
 		self.logs("INFO", "START LOAD")
 
-class ExampleApp(QtWidgets.QMainWindow, GUI.Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
+		self.end_update()
 
-		self.logs("INFO", "START")
-		print("eqweqwe")
+	def end_update(self):
+		self.hide()
+		self.window2 = ExampleApp()
+		self.window2.show()
 
 
 	def logs(self, type_log: Literal["info", "INFO", "WARN", "ERROR"], theme_log="none", text_log=""):
@@ -43,6 +41,46 @@ class ExampleApp(QtWidgets.QMainWindow, GUI.Ui_MainWindow):
 		with open(f"logs/{time.strftime('%Y-%m-%d')}.log", "a", encoding="utf-8") as log_wr:
 			log_wr.write(f'{data_log}\n')
 		print(data_log)
+
+	def mousePressEvent(self, event):
+		try:
+			if event.button() == QtCore.Qt.LeftButton:
+				self.old_pos = event.pos()
+		except:
+			pass
+
+	def mouseReleaseEvent(self, event):
+		try:
+			if event.button() == QtCore.Qt.LeftButton:
+				self.old_pos = None
+		except:
+			pass
+
+	def mouseMoveEvent(self, event):
+		try:
+			if not self.old_pos:
+				return
+			delta = event.pos() - self.old_pos
+			self.move(self.pos() + delta)
+		except:
+			pass
+
+class ExampleApp(QtWidgets.QMainWindow, GUI.Ui_MainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+
+		self.logs("INFO", "START")
+
+
+
+	def logs(self, type_log: Literal["info", "INFO", "WARN", "ERROR"], theme_log="none", text_log=""):
+		time_ = time.strftime("%H:%M:%S")
+		data_log = f'[{time_}] <{type_log}> [{theme_log}]{text_log}'
+		with open(f"logs/{time.strftime('%Y-%m-%d')}.log", "a", encoding="utf-8") as log_wr:
+			log_wr.write(f'{data_log}\n')
+		print(data_log)
+		self.textBrowser_5.append(f'[{time_}] <{type_log}> [{theme_log}]{text_log}')
 
 
 if __name__ == '__main__':
