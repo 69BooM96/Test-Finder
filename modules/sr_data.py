c6f49f38e39bbc19a9f49fa3c00bf2c78964607f
69BooM96 <tankionlaun524@gmail.com>
2024-10-30 13:20:43 +0200

    Core update

--- modules/sr_data.py
+++ modules/sr_data.py
@@ -5,16 +5,17 @@ import json
 import threading
 import Core
 
-def plugin_data(self, subject="/geografiya/", klass=None, q="вулканізм", storinka=(1, 2), proxy=None):
+def plugin_data(self, subject=None, klass=None, q=None, storinka=(1, 2), proxy=None):
 	self.log_signal.emit("INFO", f"Start_search", f" [Text][{self.mainwindows.text_search}]")
 	big_start_time = time.perf_counter()
 	plugins_list = [name for name in os.listdir("plugins")]
-	list_data_urls = []
+	self.urls_data_list = []
 
 	for pl_index, pl_name in enumerate(plugins_list, start=1):
 		self.log_signal.emit("INFO", f"Plugin", f" [{pl_index}]/[{len(plugins_list)}] [{pl_name}] [start]")
 		start_time = time.perf_counter()
 		try:
+			urls_lists = []
 			with open(f"plugins/{pl_name}/metadata.json") as metadata:
 				mt_data = json.load(metadata)
 
@@ -38,16 +39,13 @@ def plugin_data(self, subject="/geografiya/", klass=None, q="вулканізм"
 					elif data_info_pl['search']['storinka'][1] == False and storinka == None: pass
 					elif data_info_pl['search']['proxy'][1] == False and proxy == None: pass
 					else:
-						urls_lists = []
 						if data_info_pl['search']['cookie'][0]:
 							session_pl = plugin.Load_data(json.load(open(f"data/cookies/{pl_name}", "r")))
 							urls_lists = session_pl.search(**args_pl)
-							list_data_urls.append({"platform": pl_name, "urls": urls_lists})
+							self.urls_data_list.append({"platform": pl_name, "urls": urls_lists})
 						else:
 							urls_lists = plugin.Load_data.search(self, **args_pl)
-							list_data_urls.append({"platform": pl_name, "urls": urls_lists})
-
-					# print(list_data_urls)
+							self.urls_data_list.append({"platform": pl_name, "urls": urls_lists})
 
 				elif mt_data['type'] == "search_engine":
 					pass
@@ -57,6 +55,10 @@ def plugin_data(self, subject="/geografiya/", klass=None, q="вулканізм"
 		except Exception as e:
 			self.log_signal.emit("ERROR", f"Plugin", f" [{pl_index}]/[{len(plugins_list)}] [{pl_name}] [{e}]")
 	self.log_signal.emit("INFO", f"Stop_search", f" [endTime][{time.perf_counter() - big_start_time:.02f}]s")
+	# print(list_data_urls)
+	self.urls_data_list
 
-def plugin_processing_data(self, proxy=None):
+def plugin_processing_data(self, index_session=None, list_urls=None, proxy=None):
+	print(index_session, list_urls)
 	self.log_signal.emit("INFO", f"Start_load", f" [urls][]")
+	return 0
