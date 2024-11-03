e22549072d69585db451f00a509a02e687ae9435
adavidys <133536771+adavidys@users.noreply.github.com>
2024-11-03 16:12:37 +0200

    BaseException

--- modules/decorate.py
+++ modules/decorate.py
@@ -4,9 +4,9 @@ from fake_useragent import UserAgent
 def async_session(cookies):
 	def wrappers(funk):
 		async def wrapper(*agrs, **kwagrs):
-
-			async with aiohttp.ClientSession(headers={"user-agent": UserAgent().random}, timeout=aiohttp.ClientTimeout(15), cookies=cookies) as session:
-				return await funk(session, *agrs, **kwagrs)
-
+			try:
+				async with aiohttp.ClientSession(headers={"user-agent": UserAgent().random}, timeout=aiohttp.ClientTimeout(15), cookies=cookies) as session:
+					return await funk(session, *agrs, **kwagrs)
+			except BaseException as ex: return ex 
 		return wrapper
 	return wrappers
