3b2bf4f7ba34350414952c01c93899fdc85c2135
adavidys <133536771+adavidys@users.noreply.github.com>
2025-03-26 14:52:03 +0200

    обновление декоратора

--- modules/decorate.py
+++ modules/decorate.py
@@ -7,28 +7,27 @@ from threading import Thread
 from fake_useragent import UserAgent
 
 
-async def _on_request_start(session, trace_config_ctx, params, log_func):
-    request_data = {
-        "event": "request_started",
-        "method": params.method,
-        "url": str(params.url),
-        "headers": dict(params.headers),
-        "cookies": {key: value.value for key, value in session.cookie_jar.filter_cookies(params.url).items()}
-    }
-    if log_func: log_func(json.dumps(request_data, indent=4, ensure_ascii=False))
-
-async def _on_request_end(session, trace_config_ctx, params, log_func):
-    response_data = {
-        "event": "request_ended",
-        "url": str(params.url),
-        "status": params.response.status,
-        "headers": dict(params.response.headers),
-        "cookies": {key: value.value for key, value in session.cookie_jar.filter_cookies(params.url).items()},
-        "body": await params.response.text()  # Читаем тело ответа
-    }
-    if log_func: log_func(json.dumps(response_data, indent=4, ensure_ascii=False))
-
 def async_session(cookies: Optional[Dict[str, str]] = None, headers: Optional[Dict[str, str]] = None, log_func=print):
+    async def _on_request_start(session, trace_config_ctx, params, log_func):
+        request_data = {
+            "event": "request_started",
+            "method": params.method,
+            "url": str(params.url),
+            "headers": dict(params.headers),
+            "cookies": {key: value.value for key, value in session.cookie_jar.filter_cookies(params.url).items()}
+        }
+        if log_func: log_func(request_data)
+
+    async def _on_request_end(session, trace_config_ctx, params, log_func):
+        response_data = {
+            "event": "request_ended",
+            "url": str(params.url),
+            "status": params.response.status,
+            "headers": dict(params.response.headers),
+            "cookies": {key: value.value for key, value in session.cookie_jar.filter_cookies(params.url).items()},
+        }
+        if log_func: log_func(response_data)
+
     def wrappers(funk: Callable):
         async def wrapper(*args, **kwargs):
             trace_config = aiohttp.TraceConfig()
@@ -36,14 +35,17 @@ def async_session(cookies: Optional[Dict[str, str]] = None, headers: Optional[Di
             trace_config.on_request_end.append(lambda *args: _on_request_end(*args, log_func=log_func))
 
             async with aiohttp.ClientSession(
-                headers=headers or {"user-agent": UserAgent().random},
-                timeout=aiohttp.ClientTimeout(15),
-                cookies=cookies,
-                trace_configs=[trace_config]
+                    headers=headers or {"user-agent": UserAgent().random},
+                    timeout=aiohttp.ClientTimeout(15),
+                    cookies=cookies,
+                    trace_configs=[trace_config]
             ) as session:
                 return await funk(session, *args, **kwargs)
+
         return wrapper
+
     return wrappers
+
 
 def try_except(exp: Type[BaseException], funk=(lambda ex: print(ex))):
     def wrappers(fun):
